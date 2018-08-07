5c585f82a8a289c56f5a2fe6ce68492c1e28704b
Noe Lomeli <nolomeli@csumb.edu>
2018-08-07 13:13:41 -0700

    Add paralaxing

---
+++ Sinistro/Assets/Scripts/Parallaxing.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallaxing : MonoBehaviour {
+
+    public Transform[] backgrounds;
+    private float[] parallaxScales;  // The proportion of the camera's movement to move the background by. 
+    public float smoothing = 1f;    // How smooth the parallax is going to be. Make sure to set this above 0.
+
+    private Transform cam;          // reference to the main camera's transform.
+    private Vector3 previousCamPos; // The position of the camera in the previous frame.
+
+    private void Awake()
+    {
+        cam = Camera.main.transform;
+    }
+
+    // Use this for initialization
+    void Start () 
+    {
+        previousCamPos = cam.position;
+
+        parallaxScales = new float[backgrounds.Length];
+
+        for (int i = 0; i < backgrounds.Length; i++)
+        {
+            parallaxScales[i] = backgrounds[i].position.z * -1;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        for (int i = 0; i < backgrounds.Length; i++)
+        {
+            float parallax = (previousCamPos.x - cam.position.x) * parallaxScales[i];
+            float backgroundTargetPosX = backgrounds[i].position.x + parallax;
+
+            // create a target position which is the background's current position with its target's position
+            Vector3 backgroundTargetPos = new Vector3(backgroundTargetPosX, backgrounds[i].position.y, backgrounds[i].position.z);
+
+            // fade between current position and target position using Lerp
+            backgrounds[i].position = Vector3.Lerp(backgrounds[i].position, backgroundTargetPos, smoothing * Time.deltaTime);
+        }
+        previousCamPos = cam.position;
+	}
+}
