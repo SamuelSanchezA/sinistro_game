9a9090a226f8b6f1e3b483fa85606a610eefcbb6
Noe Lomeli <nolomeli@csumb.edu>
2018-08-08 17:44:28 -0700

    Add Game Over menu

---
+++ Sinistro/Assets/Scripts/GameOverUI.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOverUI : MonoBehaviour {
+
+    public void Quit()
+    {
+        Debug.Log("Application Quit!");
+        Application.Quit();
+    }
+
+    public void Retry()
+    {
+        Debug.Log("Application Retry!");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
